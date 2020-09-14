dde51c361cdae63b05dc2b99d5b282ba00fcee7d
nogari03 <mmgg555@naver.com>
2020-09-14 12:48:15 +0900

    spring - react int, encoding utf-8, maven wrapper added

--- src/main/java/project/HomeController.java
+++ src/main/java/project/HomeController.java
@@ -8,32 +8,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
-/**
- * Handles requests for the application home page.
- */
-@Controller
+@RestController
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home.jsp";
+
+	@GetMapping("/api/hello")
+	public String hello() {
+		return "안녕하세요. 현재 서버시간은 " +new Date() + "입니다. \n";
 	}
 	
 	
