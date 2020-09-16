6f0b4c783948c6adc6d7cce2d386a22ac947426b
nogari03 <mmgg555@naver.com>
2020-09-16 16:57:39 +0900

    update vo, sql query

--- src/main/java/project/group/vo/GroupVO.java
+++ src/main/java/project/group/vo/GroupVO.java
@@ -3,6 +3,7 @@ package project.group.vo;
 import lombok.*;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Component
@@ -10,20 +11,21 @@ import java.util.Date;
 public class GroupVO {
 
     private int groupNum;
-    private String groupName;
-    private int groupType;                  // groupType: 1: instance 2: San Moim
-    private int groupPeopleCount;           // groupPeopleCount : default 1
-    private String groupDetail;
-    private int groupStatus;                // groupStatus (instance) 0: end 1: normal
-    private Date groupEnrollDate;           // groupEnrollDate : default sysdate
-    private Date groupModDate;
-    private String groupArea;               /* groupArea : '서울','부산','대구','인천','광주','대전',
-                                                '울산','세종','경기',강원','충북','충남'
-                                                '전북','전남','경북','경남','제주' */
-    private int groupAgeStart;              // groupAgeStart : default 0
-    private int groupAgeEnd;                // groupAgeEnd : default 100
-    private int groupSex;                   // groupSex : 0 = nope, 1 = man, 2 = woman
+    private String name;
+    private int type;                  // type: 1=instance, 0=not instance
+    private int staffCurrent;           // staffCurrent : default 1
+    private int staffMax;
+    private String detail;
+    private int status;                // status 0: end 1: normal
+    private Date startDay;
+    private LocalDateTime createdAt;    // default : sysdate
+    private LocalDateTime updatedAt;
+    private int area;               /* groupArea : '서울=1','부산=2','대구=3','인천=4','광주=5','대전=6',
+                                                '울산=7','세종=8','경기=9',강원=10','충북=11','충남=12'
+                                                '전북=13','전남=14','경북=15','경남=16','제주=17' */
+    private int ageStart;              // groupAgeStart : default 0
+    private int ageEnd;                // groupAgeEnd : default 100
+    private int sex;                   // groupSex : 0 = nope, 1 = man, 2 = woman
     private String mtNum;
-    private Date groupStartDate;
 
 }
